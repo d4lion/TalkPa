8a3f0a1df92dc7434de4720b4399b280cb309b28
daliondev <d4lion.dev@gmail.com>
2023-02-01 21:02:59 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import Messagebar from "./components/messagebar/MessageBar"
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <h1>TalkPa</h1>
+      </nav>
+
+      <section>
+        <div></div>
+      </section>
+      <div>
+        <Messagebar />
+      </div>
+    </div>
+  )
+}
+
+export default App
