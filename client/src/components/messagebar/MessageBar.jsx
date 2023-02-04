df555f72bf7f55f6eb3674c44c1d74074a69d4af
daliondev <d4lion.dev@gmail.com>
2023-02-03 19:33:17 -0500

    feat(socket)!: send message to server

--- client/src/components/messagebar/MessageBar.jsx
+++ client/src/components/messagebar/MessageBar.jsx
@@ -1,20 +1,25 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import config from "../../constants/config.js"
+import io from "socket.io-client"
 
-export const MessageBar = () => {
+const socket = io(`${config.url}:${config.port}`)
+
+const MessageBar = () => {
   const [message, setmessage] = useState("")
 
   // Este handle busca el evaluar el momento en que se genere un cambio para poner de manera temporal un valor
   // a el mensaje que va a ser enviado
 
-  function handleChange(e) {
-    setmessage(e.target.value)
-  }
-
   function handleClick(e) {
     e.preventDefault()
-    console.log(message)
+    message != "" ? socket.emit("message", message) : ""
     setmessage("")
   }
+
+  //--------------------------------------------------------------------//
+  // Se mantiene a la espera de que un socket dispare el evento para    //
+  // Poder enviar el mensaje a los demas sockests y que estos reflejen  //
+  //--------------------------------------------------------------------//
 
   return (
     <>
@@ -23,9 +28,12 @@ export const MessageBar = () => {
           type="text"
           id="mensaje"
           name="mensaje"
-          onChange={handleChange}
+          onChange={(e) => {
+            setmessage(e.target.value)
+          }}
           value={message}
           className=" text-black flex-grow p-1 bg-slate-100"
+          maxLength={20}
         />
         <button onClick={handleClick} className="px-16 bg-slate-900 text-white">
           Send
