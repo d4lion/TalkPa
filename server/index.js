e4d38ea596db3c63c8214da17925f2ca7c923de9
daliondev <d4lion.dev@gmail.com>
2023-02-09 21:39:01 -0500

    feat(log): data displayed in server console

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import morgan from "morgan"
 import express from "express"
 import { PORT, url } from "./config.js"
 import { Server as SocketServer } from "socket.io"
+import moment from "moment"
 
 const app = express()
 const server = http.createServer(app)
@@ -18,17 +19,26 @@ app.use(cors())
 app.use(morgan("dev"))
 
 io.on("connection", (socket) => {
-  console.log("User connected: ", socket.id)
+  const userAgent = socket.request.headers["user-agent"]
+    .split("(")[1]
+    .split(" ")[0]
+    .replace(";", "")
+
+  const timeNow = moment().format("hh:mm:ss")
+
+  console.log(`${timeNow}: ${socket.id} connected on ${userAgent}`)
 
   socket.on("message", ({ content, user }) => {
     socket.broadcast.emit("message", {
       content: content,
       user:
-        user == "Anonymous" ? "Anonymous-" + socket.id.substring(0, 3) : user,
+        user == "Anonymous"
+          ? `Anonymous- ${socket.id.substring(0, 2)} (${userAgent}) `
+          : ` ${user} (${userAgent})`,
     })
   })
 })
 
 server.listen(PORT, () => {
-  console.log("Server runnin on IP:", url)
+  console.log("\n Server runnin on IP: ", url, "\n")
 })
